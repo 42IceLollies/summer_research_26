28b9be4519b6c1a6514c1f66845141f34c8f02da
Cordelia <cct2223@gmail.com>
2026-06-15 12:30:38 -0700

    Finally implementing lcLevy library

---
+++ lcLevy/Image.py
@@ -0,0 +1,14 @@
+import parse_cr3 as parse
+
+class Img():
+    
+    def __init__(self, data):
+        self.data = data
+        returned = parse.parse_image(data)
+        self.cr3 = returned["cr3"]
+        self.big_crx = returned["big_crx"]
+        self.small_crx = returned["small_crx"]
+
+
+img = Img("EOS_R100/IMG_0004.CR3")
+print(img.cr3)
